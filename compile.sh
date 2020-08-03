7f294599a38299f3d5bf9385effcdfbf1078e59e
lyceris-chan <ghostdrain@outlook.com>
2020-08-03 15:38:34 +0200

    compile.sh: initial compile script

---
+++ compile.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+# Copyright (C) 2020 Lacia chan / Lyceris chan <ghostdrain@outlook.com>
+# Copyright (C) 2018 Harsh 'MSF Jarvis' Shandilya
+# Copyright (C) 2018 Akhil Narang
+# SPDX-License-Identifier: GPL-3.0-only
+
+# Setup compile enviroment
+FILE="cscripts/README.md"
+if [ -f "$FILE" ]; then
+    cd cscripts
+    sh ./proton-clang.sh
+    cd ..
+else
+    git clone https://github.com/Daisy-Q-sources/scripts cscripts
+    cd cscripts
+    sh ./setup_env.sh
+    cd ..
+fi
+
+# Main variables
+CORES=$(grep -c ^processor /proc/cpuinfo)
+THREAD="-j$CORES"
+KERN_IMG="$(pwd)/kernel/out/arch/arm64/boot/Image.gz-dtb"
+BUILD_START=$(date +"%s")
+DATE=`date`
+PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+COMMIT_POINT="$(git log --pretty=format:'%h : %s' -1)"
+export PATH="$(pwd)/proton-clang/bin:$PATH"
+export KBUILD_COMPILER_STRING="$($(pwd)/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')";
+
+# Clone private repo with the $BOTTOKEN and $CHATID
+FILE="$(pwd)/bapi/bottoken.txt"
+if [ -f "$FILE" ]; then
+
+# Export the BOTTOKEN and CHATID from my private repository
+BOTTOKEN="$(sed 's/BOTTOKEN = //g' $(pwd)/bapi/bottoken.txt)"
+CHATID="$(sed 's/CHATID = //g' $(pwd)/bapi/chatid.txt)"
+
+else
+
+# Clone my private repository for the CHATID and BOTTOKEN
+git clone https://github.com/Lyceris-chan/telegram_bot_api_stuff bapi
+
+# Export the BOTTOKEN and CHATID from my private repository
+BOTTOKEN="$(sed 's/BOTTOKEN = //g' $(pwd)/bapi/bottoken.txt)"
+CHATID="$(sed 's/CHATID = //g' $(pwd)/bapi/chatid.txt)"
+fi
+
+# Clean up out folder
+rm -rf out/*
+
+# Post build compilation information in $CHATID
+curl -s -X POST "https://api.telegram.org/bot$BOTTOKEN/sendMessage" -d chat_id="-$CHATID" \
+              -d "disable_web_page_preview=true" \
+              -d "parse_mode=markdown" \
+              -d text="⚙️Build for \`Daisy / Sakura (non SAR)\` started at \`$DATE\` using \`$CORES\` threads.
+
+Compiler: \`$KBUILD_COMPILER_STRING\`
+Localversion: \`$LOCALVERSION\`
+
+Branch: \`$PARSE_BRANCH\`
+Commit: \`$COMMIT_POINT\`
+
+Kernel: \`Sleepy\`"
+
+
+# Compile the kernel
+build_clang () {
+    make -j$(nproc --all) O=out \
+                          ARCH=arm64 \
+                          CC=clang \
+                          AR="llvm-ar" \
+                          NM="llvm-nm" \
+                          OBJCOPY="llvm-objcopy" \
+                          OBJDUMP="llvm-objdump" \
+                          STRIP="llvm-strip" \
+                          CROSS_COMPILE=aarch64-linux-gnu-
+}
+
+make O=out ARCH=arm64 sleepy_defconfig
+build_clang
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+
+# Post failure in channel
+
+FILE="$(pwd)/out/arch/arm64/boot/Image.gz-dtb"
+if [ -f "$FILE" ]; then
+
+# Zip it
+rm -rf $(pwd)/AnyKernel3/Image.gz-dtb
+cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+rm -rf $(pwd)/AnyKernel3/*.zip
+BUILD_TIME=$(date +"%d%m%Y-%H%M")
+cd AnyKernel3
+zip -r9 Sleepy-${BUILD_TIME}.zip *
+cd ..
+
+# Post release in channel
+curl -F document=@"$(pwd)/AnyKernel3/Sleepy-${BUILD_TIME}.zip" "https://api.telegram.org/bot$BOTTOKEN/sendDocument" \
+            -F chat_id="-$CHATID"  \
+            -F "disable_web_page_preview=true" \
+            -F "parse_mode=Markdown" \
+            -F caption="✅ Build finished in \`$(($DIFF / 60))\` minute(s) and \`$(($DIFF % 60))\` seconds"
+else
+
+curl -s -X POST "https://api.telegram.org/bot$BOTTOKEN/sendMessage" -d chat_id="-$CHATID" \
+              -d "disable_web_page_preview=true" \
+              -d "parse_mode=markdown" \
+              -d text="❌ Build failed in \`$(($DIFF / 60))\` minute(s) and \`$(($DIFF % 60))\` seconds check you're terminal"
+              
+fi
+
